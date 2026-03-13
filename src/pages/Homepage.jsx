ed3bcc8c378c7374d78afd2e7d7d6709ae5caa4c
user <dinissantanalopes@gmail.com>
2026-03-13 11:42:37 +0000

    mais cenas

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,6 +1,8 @@
 import "../assets/css/styles.css"
 import "../components/Header.jsx"
 import Header from "../components/Header.jsx";
+import Contador from "../components/Contador.jsx";
+import Slideshow from "../components/Slideshow.jsx";
 import Table from "../components/Table.jsx";
 
 const Homepage = () => {
