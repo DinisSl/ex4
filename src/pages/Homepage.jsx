0f5c7c79a87b3af6044a07baaffd43dc87f1c651
Afonso Martins <arfms4@iscte-iul.pt>
2026-03-13 11:34:08 +0000

    Com tabela

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,6 +1,7 @@
 import "../assets/css/styles.css"
 import "../components/Header.jsx"
 import Header from "../components/Header.jsx";
+import Table from "../components/Table.jsx";
 
 const Homepage = () => {
     return (
@@ -8,8 +9,9 @@ const Homepage = () => {
             <Header/>
             <main>
 
+
+                <Table/>
             </main>
-            <Footer/>
         </>
     )
 }
