def3d51d568a95f7687e7006819125d6b76e3e48
Afonso Martins <arfms4@iscte-iul.pt>
2026-03-21 17:21:51 +0000

    form estilizado

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,4 +1,5 @@
 import {useState} from "react";
+import "../assets/css/FormStyle.css"
 
 const Formulario = () => {
 
@@ -11,27 +12,29 @@ const Formulario = () => {
     }
 
     return (
-        <>
-            <h1>Qual a melhor vogal de todas?</h1>
-            {
-                opcoes.map
-                (o =>
-                    <>
-                        <label>
-                            <input
-                                name="grp1"
-                                type="radio"
-                                value={o}
-                                onChange={changeHandler}
-                            />{o}
-                        </label>
-                        <br/>
-                    </>
-                )
-            }
-            <button onClick={() => alert("A melhor vogal de todos os tempos é " + opcaoSelecionada)}>Submeter</button>
-        </>
-    )
-}
+        <div className="containerf">
+            <div className="form-box">
+                <h1>Qual a melhor vogal de todas?</h1>
+                {
+                    opcoes.map
+                    (o =>
+                        <>
+                            <label>
+                                <input
+                                    name="grp1"
+                                    type="radio"
+                                    value={o}
+                                    onChange={changeHandler}
+                                />{o}
+                            </label>
+                            <br/>
+                        </>
+                    )
+                }
+                <button onClick={() => alert("A melhor vogal de todos os tempos é " + opcaoSelecionada)}>Submeter</button>
+            </div>
+        </div>
+    );
+};
 
 export default Formulario;
